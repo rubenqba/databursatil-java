19b99a52801569ac0479417702efaf805f329457
Ruben Bresler <111847+rubenqba@users.noreply.github.com>
2025-03-21 13:38:59 -0600

    first commit

---
+++ databursatil-api-sdk/src/main/java/com/github/rubenqba/databursatil/DatabursatilApiSdkApplication.java
@@ -0,0 +1,13 @@
+package com.github.rubenqba.databursatil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabursatilApiSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabursatilApiSdkApplication.class, args);
+    }
+
+}
---
+++ databursatil-api-sdk/src/test/java/com/github/rubenqba/databursatil/DatabursatilApiSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rubenqba.databursatil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabursatilApiSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
